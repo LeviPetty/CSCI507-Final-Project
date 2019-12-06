1ce08c8bbb11a30cfe5f8cd56dfc9d8090c43123
LeviPetty <lpetty@mymail.mines.edu>
2019-12-05 21:28:56 -0700

    Removed commented-out code from sift.m

--- sift.m
+++ sift.m
@@ -10,26 +10,6 @@ function doesMatch = sift(baseDir, baseFiles, compDir, compFiles)
         run(fullfile(siftDir, 'vl_setup'));
     end
 
-%     if exist('baseDir', 'var') == 0 | exist('baseFiles', 'var') == 0
-%         disp('Please select folder containing the first image');
-%         baseDir = uigetdir;
-%         baseFiles = dir(fullfile(baseDir, '*.jpg'));
-%     elseif input('Is the folder containing the first image the same as before? (y/n)', 's') ~= 'y'
-%         disp('Please select folder containing the first image');
-%         baseDir = uigetdir;
-%         baseFiles = dir(fullfile(baseDir, '*.jpg'));
-%     end
-% 
-%     if exist('compDir', 'var') == 0 | exist('compFiles', 'var') == 0
-%         disp('Please select folder containing the second image');
-%         compDir = uigetdir;
-%         compFiles = dir(fullfile(compDir, '*.jpg'));
-%     elseif input('Is the folder containing the second image the same as before? (y/n)', 's') ~= 'y'
-%         disp('Please select folder containing the second image');
-%         compDir = uigetdir;
-%         compFiles = dir(fullfile(compDir, '*.jpg'));
-%     end
-
     doesMatch = [];
     for i = 1:length(baseFiles)
         baseImg = imread(fullfile(baseDir, baseFiles(i).name));
